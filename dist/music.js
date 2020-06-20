0210b3ec1371d4a9ea0d01a4d13d407aa8ea3fb5
HxWGuang <hxguang99@outlook.com>
2020-06-20 12:08:49 +0800

    Site updated: 2020-06-20 12:08:39

---
+++ dist/music.js
@@ -0,0 +1,55 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "Can't Get You out of My Head",
+        artist: 'Glimmer of Blooms',
+        url: 'https://music.163.com/song/media/outer/url?id=1326862369.mp3',
+        cover: '',
+      },
+      {
+        name: 'это ли счастье?',
+        artist: 'Rauf & Faik',
+        url: 'https://music.163.com/song/media/outer/url?id=1359663981.mp3',
+        cover: '',
+      },
+      {
+        name: '你说呢',
+        artist: '六哲',
+        url: 'https://music.163.com/song/media/outer/url?id=864255393.mp3',
+        cover: '',
+      },
+      {
+        name: '余香（翻自 张小九）',
+        artist: '晚安民谣',
+        url: 'https://music.163.com/song/media/outer/url?id=1431636975.mp3',
+        cover: '',
+      },
+      {
+        name: 'MELANCHOLY',
+        artist: 'White Cherry',
+        url: 'https://music.163.com/song/media/outer/url?id=1297742167.mp3',
+        cover: '',
+      },
+      {
+        name: '怎么能忘了（不插电）',
+        artist: '程响',
+        url: 'https://music.163.com/song/media/outer/url?id=1416772486.mp3',
+        cover: '',
+      },
+      {
+        name: '落霜',
+        artist: '花粥',
+        url: 'https://music.163.com/song/media/outer/url?id=1358035181.mp3',
+        cover: '',
+      },
+      {
+        name: '琴师（翻自 音频怪物）',
+        artist: '要不要买菜',
+        url: 'https://music.163.com/song/media/outer/url?id=1406119516.mp3',
+        cover: '',
+      }
+    ]
+});
